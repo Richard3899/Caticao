40f97cb94038eb43a30081ee5978c5cada731a1d
Richard Ronny Arias Marreros <richard.arias3899@gmail.com>
2021-10-12 18:46:06 -0500

    creando composer

---
+++ ProyectoCaticao-ULT/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '873a8d733f4a61b1769c9cf120650c3a1bad61b4',
+        'name' => 'richard/proyecto-caticao-ult',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'richard/proyecto-caticao-ult' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '873a8d733f4a61b1769c9cf120650c3a1bad61b4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
